71091028d6c7310069a035234831c25bca6ee6ac
Nada hamdani <nada.hamdani.nh@gmail.com>
2023-10-05 13:10:41 -0700

    using printf to print to the standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ * return  value of main indicates a successful execution.
+*/
+int main(void)
+{
+const char *Text = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", Text);
+return (0);
+}
