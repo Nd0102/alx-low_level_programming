61cd5cf6036627640cb5837d4c1284a536344ffd
Nada hamdani <nada.hamdani.nh@gmail.com>
2023-10-06 13:13:45 -0700

    Write a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+short i = 48;
+short Counter = 0;
+while (i <= 57)
+{
+
+short j = 48;
+while (j <= 57)
+{
+
+if (j == 48)
+{
+++Counter;
+j += Counter;
+}
+else
+{
+putchar(i);
+putchar(j);
+
+if (Counter < 9)
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+
+}
+i++;
+}
+
+putchar('\n');
+return (0);
+}
