eff39830a2684f308d4bca65f54e782d8b4eec73
Nada hamdani <nada.hamdani.nh@gmail.com>
2023-11-08 12:38:59 -0800

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+
 /**
- * print_name - prints a name
- * @name: name to be printed(pointer to a string)
- * @f: func pointer to a func that prints a string
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
  *
- * this functions takes a @name (pointer to a string)
- * and a function pointer @f as params.
- * checks if @f or @name are NULL
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
-
-	((name == NULL) || (f == NULL) ? exit(6) : f(name));
+	if (name && f)
+		f(name);
 }
-
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION-POINTERS_H
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
 #include <stdlib.h>
 
